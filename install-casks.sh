24ee9fe367bebb17018f8a3989d3a23f96bc807f
Logan Barnett <logustus@gmail.com>
2016-08-18 11:32:23 -0700

    add diffmerge cask

--- install-casks.sh
+++ install-casks.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # TODO: add alfred and istat-menus
-CASKS="slack chromium discord skype hipchat microsoft-lync mumble screenhero silverlight google-chrome"
+CASKS="slack chromium discord skype hipchat microsoft-lync mumble screenhero silverlight google-chrome diffmerge"
 
 echo "installing homebrew casks"
 brew cask install $CASKS
