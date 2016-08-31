e10037891818767dddb44d266bdeb8f743b11512
Logan Barnett <logustus@gmail.com>
2016-08-31 11:18:03 -0700

    add robomongo to casks

--- install-casks.sh
+++ install-casks.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # TODO: add alfred and istat-menus
-CASKS="slack chromium discord skype hipchat microsoft-lync mumble screenhero silverlight google-chrome diffmerge"
+CASKS="slack chromium discord skype hipchat microsoft-lync mumble screenhero silverlight google-chrome diffmerge robomongo"
 
 echo "installing homebrew casks"
 brew cask install $CASKS
