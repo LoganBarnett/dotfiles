90a4c35b4ce5cd494ced96152311413e0d1c1235
Logan Barnett <logustus@gmail.com>
2017-03-18 16:03:35 -0700

    break cask list into multiple lines

--- install-casks.sh
+++ install-casks.sh
@@ -1,7 +1,22 @@
 #!/usr/bin/env bash
 
 # TODO: add alfred and istat-menus
-CASKS="slack chromium discord skype hipchat microsoft-lync mumble screenhero silverlight google-chrome diffmerge robomongo steam virtualbox"
+CASKS="
+slack
+chromium
+discord
+skype
+hipchat
+microsoft-lync
+mumble
+screenhero
+silverlight
+google-chrome
+diffmerge
+robomongo
+steam
+virtualbox
+"
 
 echo "installing homebrew casks"
 brew cask install $CASKS
