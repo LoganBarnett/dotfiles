b6c26b19455c09a04d2123f93865c876605d9774
Logan Barnett <logustus@gmail.com>
2017-05-02 14:01:38 -0700

    add error handling for pushing injections to safari, and clean up comments/logs

--- write-safari-css.js
+++ write-safari-css.js
@@ -9,6 +9,7 @@ const readFileData = (path) => {
   try {
     return fs.readFileSync(path, 'utf8')
   }
+  // TODO: Specifically look for files not existing as a catch.
   catch(e) {
     return ''
   }
@@ -32,21 +33,13 @@ names.forEach(name => {
 
   meta.key = meta.key || name
 
-// const keySql = `select key from ItemTable;`
-// // TODO: error handling
-// const getKeyProc = childProcess.spawnSync('sqlite3', [sqlitePath, keySql])
-// const keys = getKeyProc.stdout.toString().split('\n')
-
   const css = readFileData(`custom-css/${name}.css`, 'utf8')
   let js = readFileData(`custom-css/${name}.js`, 'utf8')
   if(js == 'undefined') js = ''
 
-// const metaText = fs.readFileSync(`custom-css/${name}.json`, 'utf8')
-// const meta = JSON.parse(metaText)
   meta.styles = css
   meta.script = js
 
-  // console.log('sending meta', meta)
   const convertedMeta = new Iconv('UTF-8', 'UTF-16LE')
       .convert(JSON.stringify(meta))
 
@@ -64,7 +57,7 @@ names.forEach(name => {
     files.find(f => f.match(findSqliteFile))
   )
 
-    const updateSql = `
+  const updateSql = `
 pragma encoding = "UTF-8";
 delete from itemtable where key='${meta.key}';
 insert into itemtable
@@ -72,10 +65,18 @@ insert into itemtable
 ('${meta.key}', readfile('.tmp-css-write/${meta.name}.json'))
 ;`
 
-    // console.log(updateSql)
-    const sqliteProcess = childProcess.spawn('sqlite3', [
-      sqlitePath,
+  // console.log(updateSql)
+  const sqliteProcess = childProcess.spawnSync('sqlite3', [
+    sqlitePath,
+    updateSql,
+  ])
+
+  if(sqliteProcess.status != 0) {
+    console.error(
+      'Error executing update of injection',
       updateSql,
-    ])
+      sqliteProcess.stderr.toString()
+    )
+  }
 })
 console.log('success')
