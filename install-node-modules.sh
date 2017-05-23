44aea88ee94fc1e06a5014e50e637e475cd6f83d
Logan Barnett <logustus@gmail.com>
2017-05-23 10:51:46 -0600

    add elm utilities

--- install-node-modules.sh
+++ install-node-modules.sh
@@ -1,7 +1,24 @@
 #! /usr/bin/env bash
 
+# avn switches the node version using n based on a .node-version when
+# cd/pushd/popd into a directory.
 # tern for JS autocomplete and other hinting
-NODE_MODULES="tern marked bower avn avn-nvm iconv"
+# elm-format forces 4 spaces (they claim this is because they are alpha) - bleh.
+# https://github.com/avh4/elm-format/issues/226
+# elm-test is a unit/fuzz testing framework.
+# https://github.com/elm-community/elm-test
+# iconv used for encoding conversion to Safari's local storage.
+NODE_MODULES="
+avn
+avn-nvm
+bower
+elm-format
+elm-oracle
+elm-test
+iconv
+marked
+tern
+"
 
 npm i -g $NODE_MODULES
 
