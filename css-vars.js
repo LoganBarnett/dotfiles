60d411dc00f8df8b11dcab6e1c74e06bc2bf9bb0
Logan Barnett <logustus@gmail.com>
2017-05-27 18:02:07 -0600

    darken the some tabs in habitica

--- css-vars.js
+++ css-vars.js
@@ -3,5 +3,20 @@ module.exports = {
   '--text': '#b2b2b2',
   '--dim-text': '#686868',
   '--background': '#292b2e',
+  '--general-background-color': '#292b2e',
   '--link-color': '#2d9574',
+  '--heading-background-color': '#292b2e',
+  '--tab-inactive-color': '#2aa1ae',
+  '--tab-inactive-background-color': '#292b2e',
+  '--tab-left-inactive-border-radius': '0 0 0.5em 0',
+  '--tab-right-inactive-border-radius': '0 0 0 0.5em',
+  // Setting the container's background-color makes it so we get the S shaped
+  // border radius on the tabs, because the border-radius on the bottom allows
+  // the background-color of the container to leak through.
+  '--tab-inactive-sibling-container-background-color': '#5d4d7a',
+  '--tab-active-background-color': '#5d4d7a',
+  '--tab-active-border-radius': '0.5em 0.5em 0 0',
+  '--tab-active-color': '#b2b2b2',
+  '--tab-list-background-color': '#292b2e',
+  '--button-background-color': '',
 }
