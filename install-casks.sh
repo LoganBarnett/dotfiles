211d7c7cf9336298af4862d438b1643322585e43
Logan Barnett <logustus@gmail.com>
2017-06-24 20:37:07 -0700

    complain about intrusive razer drivers

--- install-casks.sh
+++ install-casks.sh
@@ -1,6 +1,13 @@
 #!/usr/bin/env bash
 
+
+# drivers is necessary for tools such as razer-synapse
+echo "tapping drivers"
+brew tap caskroom/drivers
+
 # TODO: add alfred and istat-menus
+# NOTE: I omitted razer-synpase due to its intrusive nature. Install manually
+# and then uninstall when keyboard config is complete.
 CASKS="
 slack
 chromium
