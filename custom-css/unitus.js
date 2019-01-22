60c885fe78747b8cc4c4728dd41b88eef39cfcf0
Logan Barnett <logustus@gmail.com>
2019-01-22 09:19:15 -0800

    automatically proceed with answering security questions
    When on Unitus, instead of manually telling the browser to answer security
    questions, and then automatically answering the question, just flow through the
    process automatically.

--- custom-css/unitus.js
+++ custom-css/unitus.js
@@ -1,19 +1,41 @@
+function selectQuestionPath() {
+  const questionInput = document.querySelector('#sa_scm_dcrd_questionInput')
+  if(questionInput) {
+    console.log('Choosing "security" questions...')
+    questionInput.checked = true
+    const submitButton = document.querySelector('#btn_sauth_cc_submit')
+    if(submitButton) {
+      submitButton.click()
+    } else {
+      console.error('Couldn\'t find submit button!')
+    }
+  } else {
+    console.log('No security question prompt found.')
+  }
+}
+
+// You're being passive aggressive again...
+function answerQuoteSecurityQuestionUnquote() {
+  var answers = Array.from(document.querySelectorAll('[name="Answer"]'))
+  var validAnswers = process.unitusAnswers
+  var answer = answers.filter(function(n) {
+    return validAnswers.includes(n.value)
+  })[0]
+  if(answer) {
+    console.log('Selecting answer', answer)
+    answer.checked = true
+
+    var submitButton = document.querySelector('#btn_sauth_sc_submit')
+    submitButton.click()
+  }
+  else {
+    console.log('No security questions found.')
+  }
+}
+
 (function() {
   setTimeout(function() {
-    var answers = Array.from(document.querySelectorAll('[name="Answer"]'))
-    var validAnswers = process.unitusAnswers
-    var answer = answers.filter(function(n) {
-      return validAnswers.includes(n.value)
-    })[0]
-    if(answer) {
-      console.log('selecting', answer)
-      answer.checked = true
-
-      var submitButton = document.querySelector('#btn_sauth_sc_submit')
-      submitButton.click()
-    }
-    else {
-      console.log('no security questions found')
-    }
+    selectQuestionPath()
+    answerQuoteSecurityQuestionUnquote()
   }, 500)
 })()
