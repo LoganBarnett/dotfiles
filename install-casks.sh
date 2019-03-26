ec3d9e541fcee8cca646be8859c3107078abf2a2
Logan Barnett <logustus@gmail.com>
2019-03-26 07:41:40 -0700

    add fix for outdated homebrew casks
    Older cask definitions use the "undent" method on String, which has been removed
    from either later version of Homebrew or Ruby itself (I care not which applies).
    This means upgrading from older definitions is problematic because the newer
    executing (and self updating) Homebrew breaks against what was perfectly valid
    definitions in a prior version. To get around this, the undent call is stripped
    from where it is used and it is assumed that the string value in place will
    suffice.
    
    I have seen issues similar to this in the past, and I expect I will need to add
    more repair scripting as I continue to use Homebrew.

--- install-casks.sh
+++ install-casks.sh
@@ -2,6 +2,20 @@
 
 set -e
 
+# This fixes outdated tasks using the "undent" method. I've found Homebrew
+# doesn't do much to preserve backwards compatibility with older packages (or
+# more accurately: older Homebrew package definitions). I expect many
+# workarounds such as this will pile up in this file and the install-packages.sh
+# file. This particular workaround can be found (and adapted to work with this
+# sed) from here:
+# https://github.com/Homebrew/homebrew-cask/issues/49716#issuecomment-413515303
+echo "Fixing outdated Homebrew package definitions..."
+find "$(brew --prefix)/Caskroom/"*'/.metadata' -type f -name '*.rb' | \
+    xargs grep 'EOS.undent' --files-with-matches | \
+    xargs sed -i 's/EOS.undent/EOS/'
+
+echo "Outdated Homebrew package definitions should be fixed."
+
 brew cask upgrade
 
 # NOTE: I omitted razer-synpase due to its intrusive nature. Install manually
