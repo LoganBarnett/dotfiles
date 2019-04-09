de364209701fd5303b75167ebeb247753f074803
Logan Barnett <logustus@gmail.com>
2019-04-09 13:34:08 -0700

    fix an issue with a jenv upgrade
    The jenv call used to work in bash, but it's been forced to run in zsh now so it
    can make the actual changes to the shell as intended.

--- install-java.sh
+++ install-java.sh
@@ -7,4 +7,4 @@
 # Heads up that you'll need to restart the terminal session after doing this.
 # TODO: Warn at the end of the installation process that this must be done,
 # since the built-in warning will likely be lost in a sea of output.
-jenv enable-plugin export
+exec $SHELL -l -c 'jenv enable-plugin export'
