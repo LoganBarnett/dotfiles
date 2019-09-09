afc3310278037d930ef2225b18a2f66f89ba72b9
Logan Barnett <logustus@gmail.com>
2019-09-09 16:35:18 -0700

    fix issue where nodenv breaks immediately after an upgrade

--- install-node-modules.sh
+++ install-node-modules.sh
@@ -11,6 +11,14 @@ brew remove nvm || true
 # variable, NODE_BUILD_DEFINITIONS.
 ./install-package.sh nodenv/nodenv/node-build-update-defs
 
+# nodenv must be initialized in part because an upgrade will cause the old path
+# to fail.
+eval "$(nodenv init -)"
+echo 'Other shells you have will need to run the following:
+
+eval "$(nodenv init -)"
+'
+
 nodenv install -s 10.15.3
 
 # nodenv switches the node version based on a .node-version when
