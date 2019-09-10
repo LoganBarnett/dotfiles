f43407d3e5d3c7c7fb0d216099d74755dcabd806
Logan Barnett <logustus@gmail.com>
2019-09-09 17:01:03 -0700

    upgrade rubygems before upgrading gems

--- install-rubygems.sh
+++ install-rubygems.sh
@@ -6,6 +6,7 @@ GEMS="jekyll bundler"
 # 10.12+ needs a sudoer to install gems. All of my systems are on 10.12+ so just
 # sudo by default.
 echo "Need sudoer access to install gems."
+sudo gem update --system
 sudo gem install $GEMS
 
 echo "github-pages needs a specific version to be compatible with older ruby"
