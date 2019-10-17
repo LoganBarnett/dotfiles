b1aa5712a34ba94f34dbe34700be490a9afb49d0
Logan Barnett <logustus@gmail.com>
2019-10-17 15:12:28 -0700

    fix jenv plugin configuration
    It wasn't adding plugins correctly because of running without zshrc.

--- install-java.sh
+++ install-java.sh
@@ -14,4 +14,7 @@ jenv add /Library/Java/JavaVirtualMachines/adoptopenjdk-11.jdk/Contents/Home
 # Heads up that you'll need to restart the terminal session after doing this.
 # TODO: Warn at the end of the installation process that this must be done,
 # since the built-in warning will likely be lost in a sea of output.
-exec $SHELL -l -c 'jenv enable-plugin export'
+
+# This needs the jenv init - setup stuff because we're in bash right now, and
+# we're trying to run this in zsh for zsh.
+exec $SHELL -l -c 'eval "$(jenv init -)" && jenv enable-plugin export'
