2926c3bb4bb58fa722a3412c7b24f248a5e9b88c
Logan Barnett <logustus@gmail.com>
2020-02-18 10:52:37 -0800

    use more robust error filtering for making symlinks

--- link-dotfile.sh
+++ link-dotfile.sh
@@ -3,5 +3,5 @@
 # This script moves a dotfile from this dir to ~/.dotfile.
 
 # grep used to suppress warning of symlink existing
-WARNING="ln: $HOME/.$dotfile: File exists"
+WARNING="File exists"
 ln -s -n $PWD/$1 ~/.$1 2>&1 | grep -v "$WARNING" || true
