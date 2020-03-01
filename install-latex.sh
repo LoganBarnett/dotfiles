2824bf0fd1912897a66827a4416efa28374c2242
Logan Barnett <logustus@gmail.com>
2020-03-01 08:53:05 -0800

    improve latex support
    squash! improve latex support
    
    squash! improve latex support

--- install-latex.sh
+++ install-latex.sh
@@ -3,9 +3,10 @@
 set -e
 
 echo "Installing latex support..."
-# basictex adds latex support. pandoc requires latex to export to PDF amongst
-# other things.
-brew cask install basictex
+# mactex adds latex support. pandoc requires latex to export to PDF amongst
+# other things. It also installs dvipng needed for inline latex previews.
+brew cask install mactex
 # This makes pdflatex available for pandoc to use.
 ln -snf /Library/TeX/texbin/pdflatex /usr/local/bin/pdflatex
+ln -snf /Library/TeX/texbin/dvipng /usr/local/bin/dvipng
 echo "[DONE] Installed latex support!"
