10fac48994577f03e3214006715c249a0898539c
Logan Barnett <logustus@gmail.com>
2020-06-06 14:12:31 -0700

    add latex on the PATH

--- install-latex.sh
+++ install-latex.sh
@@ -7,6 +7,7 @@ echo "Installing latex support..."
 # other things. It also installs dvipng needed for inline latex previews.
 brew cask install mactex
 # This makes pdflatex available for pandoc to use.
+ln -snf /Library/TeX/texbin/latex /usr/local/bin/latex
 ln -snf /Library/TeX/texbin/pdflatex /usr/local/bin/pdflatex
 ln -snf /Library/TeX/texbin/dvipng /usr/local/bin/dvipng
 echo "[DONE] Installed latex support!"
