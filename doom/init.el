bdb91299cf170d280b73d041bd290eb2318f3b86
Logan Barnett <logustus@gmail.com>
2020-06-18 17:37:02 -0700

    add ibuffer and javascript modules
    I like ibuffer, and this makes ibuffer come up with :ls.
    
    This JS module should introduce LSP config. I've tested this only in a
    limited way so far, since I'm mostly doing JS over SSH these days. It
    does not work with Tramp.

--- doom/init.el
+++ doom/init.el
@@ -65,7 +65,7 @@
        :emacs
        dired             ; making dired pretty [functional]
        electric          ; smarter, keyword-based electric-indent
-       ;;ibuffer         ; interactive buffer management
+       ibuffer         ; interactive buffer management
        undo              ; persistent, smarter undo for your inevitable mistakes
        vc                ; version-control and Emacs, sitting in a tree
 
@@ -126,7 +126,7 @@
        ;;idris             ;
        ;;json              ; At least it ain't XML
        ;;(java +meghanada) ; the poster child for carpal tunnel syndrome
-       ;;javascript        ; all(hope(abandon(ye(who(enter(here))))))
+       (javascript +lsp)   ; all(hope(abandon(ye(who(enter(here))))))
        ;;julia             ; a better, faster MATLAB
        ;;kotlin            ; a better, slicker Java(Script)
        ;;latex             ; writing papers in Emacs has never been so fun
