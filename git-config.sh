421d4c524f59baec639cb710336b48a7e9870fcb
Logan Barnett <logustus@gmail.com>
2021-02-23 21:06:35 -0800

    add git global commit hook for jira-like tickets
    Also cleans up a lot of git usage.

--- git-config.sh
+++ git-config.sh
@@ -1,18 +1,13 @@
+#!/usr/bin/env bash
+
 set -e
 
 # By default git uses vi, which isn't always what we want. Set it to vim
 # instead. See:
 # http://www.freyskeyd.fr/fixing-there-was-a-problem-with-the-editor-vi-for-git/
-git config --global core.editor $(which vim)
+#
+# This is now set directly in the gitconfig, but left for reference.
+# git config --global core.editor $(which vim)
 
 
-DOTFILES="
-gitattributes_global
-gitconfig
-gitignore_global
-"
-
-for dotfile in $DOTFILES
-do
-  ./link-dotfile.sh $dotfile
-done
+./link-dotfile.sh gitconfig
