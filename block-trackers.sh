2f462173bf224d13fbf1be8782dd1b24bc7b1028
Logan Barnett <logustus@gmail.com>
2022-09-18 22:15:23 -0700

    block trackers listed on disconnectme

--- block-trackers.sh
+++ block-trackers.sh
@@ -11,6 +11,17 @@ www.googleanalytics.com
 www.google-analytics.com
 "
 
+slog "Getting list from github.com/disconnectme/disconnect-tracking-protection..."
+
+blocks=$(curl \
+  -L \
+  https://raw.githubusercontent.com/disconnectme/disconnect-tracking-protection/master/entities.json \
+  |  jq -r '.entities[].resources[]' \
+)
+
+# At last count, this was up to about 3k, shew!
+slog "Got list. ${echo "$blocks" | wc -l} to block."
+
 for url in $blocks; do
   if ! grep -q $url /etc/hosts; then
     sudo sh -c "echo '0.0.0.0 $url' >> /etc/hosts"
