16ff8eac4bcdd09d0f383e3bef2ac3f720bfcf72
Logan Barnett <logustus@gmail.com>
2022-09-19 15:40:36 -0700

    don't block everything
    This list needs to be refined. Blocking the resources listed here means
    many sites (such as github itself) cannot be reached. We need to block
    direct tracking sites.

--- block-trackers.sh
+++ block-trackers.sh
@@ -13,14 +13,14 @@ www.google-analytics.com
 
 slog "Getting list from github.com/disconnectme/disconnect-tracking-protection..."
 
-blocks=$(curl \
-  -L \
-  https://raw.githubusercontent.com/disconnectme/disconnect-tracking-protection/master/entities.json \
-  |  jq -r '.entities[].resources[]' \
-)
+# blocks+=$(curl \
+#   -L \
+#   https://raw.githubusercontent.com/disconnectme/disconnect-tracking-protection/master/entities.json \
+#   |  jq -r '.entities[].resources[]' \
+# )
 
 # At last count, this was up to about 3k, shew!
-slog "Got list. ${echo "$blocks" | wc -l} to block."
+# slog "Got list. ${echo "$blocks" | wc -l} to block."
 
 for url in $blocks; do
   if ! grep -q $url /etc/hosts; then
