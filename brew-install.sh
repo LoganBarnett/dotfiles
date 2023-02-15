a31d87d0cc5c82db57ee996635775be2cee1b6a9
Logan Barnett <logustus@gmail.com>
2023-02-15 10:35:27 -0800

    use Homebrew's new installer

--- brew-install.sh
+++ brew-install.sh
@@ -4,14 +4,19 @@ source bash-logging
 
 dir="$(dirname "${BASH_SOURCE[0]}")"
 
+# Make sure we actually fail here. Homebrew changes things up frequently by my
+# standards, up to even rewriting their install scripts.
+set -euo pipefail
+
 if [ $(uname) = 'Darwin' ]; then
   cd $dir
   slog "Installing homebrew..."
   # Redirect stdin from /dev/null to put the script into a non-interactive
   # mode.
-  /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)" < /dev/null
+  /bin/bash -e \
+    "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)" \
+    < /dev/null
   slog "Installing casks..."
-  export PATH="$PATH:/opt/homebrew/bin"
   $dir/brew-cask-install.sh
 else
   slog "Skipping cask installs - not osx."
