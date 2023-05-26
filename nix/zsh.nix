db071b4d069263118b14121cdf16fa04aa604102
Logan Barnett <logustus@gmail.com>
2023-05-26 12:45:28 -0700

    clean up zshrc and move into nix

--- nix/zsh.nix
+++ nix/zsh.nix
@@ -19,6 +19,7 @@
     plugins = [
       #"nix"
     ];
+    theme = "robbyrussell";
   };
   loginExtra = ''
       source ~/.zshenv-customized
@@ -27,4 +28,28 @@
       source ${pkgs.zsh-git-prompt}/share/zsh-git-prompt/zshrc.sh
       source ~/.zshrc-customized
     '';
+  shellAliases = {
+    # -G is BSD, but with Nix we use --color now.
+    ls="ls -aG --color=auto";
+    grep="grep --color=auto";
+    b="bundle exec";
+    curl-json="curl -v -H 'Accept: application/json'";
+
+    # git
+    gs="git status";
+    gfp="git push --force-with-lease";
+    grc="git rebase --continue";
+    gro="git restore --ours";
+    grt="git restore --theirs";
+    glp="git log --pretty=format:'%Cred%h%Creset %<(60,trunc)%s %Cgreen%<(12,trunc)%cr %C(bold blue)%<(12,trunc)%an%Creset %C(yellow)%<(20,mtrunc)%d%Creset' --abbrev-commit";
+    gbu="git branch --set-upstream-to=origin/$(git branch --show-current) $(git branch --show-current)";
+
+    # ripgrep
+    rgh="rg --hidden --glob '!.git'"; # Search hidden files.
+
+    # yarn flow management because it happens a lot.
+    yf="yarn flow";
+    yfs="yarn flow stop";
+    yfr="yfs && yf";
+  };
 }
