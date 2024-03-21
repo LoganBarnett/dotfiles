853d339d751020398e4a5d640e408b6b85e8cb81
Logan Barnett <logustus@gmail.com>
2024-03-20 17:34:02 -0700

    fix nix store optimization (don't)
    This was causing problems with lithium's store.  Just remove it.

--- nix/nixos-modules/nix-store-optimize.nix
+++ nix/nixos-modules/nix-store-optimize.nix
@@ -4,6 +4,14 @@
 { ... } : {
   nix.settings = {
     # Deduplicate and optimize nix store.
-    auto-optimise-store = true;
+    # There might be some assumptions here that are not safe.  All I have on
+    # that presently is this post:
+    # https://realjenius.com/2023/07/21/corrupt-store/
+    # It claims other tickets are present that worry about it.  I have found
+    # https://github.com/NixOS/nix/issues/1281 but it does not seem like hard
+    # evidence.  I have had broken hashes during a `nix-store --verify
+    # --check-contents`.  I also have other compiler errors.  I've had physical
+    # memory issues in the past, but those have passed lots of tests.
+    auto-optimise-store = false;
   };
 }
