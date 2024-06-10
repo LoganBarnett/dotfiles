cfb23cdef00ea0f9988fe6626f387c1b278535eb
LoganBarnett <logustus@gmail.com>
2024-06-09 23:05:54 -0700

    get openldap working on nickel

--- nix/nixos-modules/ldap-server.nix
+++ nix/nixos-modules/ldap-server.nix
@@ -3,6 +3,8 @@
     generator.script = "passphrase";
     # Always specify the rekey file or it goes into a weird directory?
     rekeyFile = ../secrets/ldap-root-pass.age;
+    group = "openldap";
+    mode = "0440";
   };
   services.openldap = {
     enable = true;
@@ -19,30 +21,32 @@
     # This has some very good information on the topic:
     # https://www.zytrax.com/books/ldap/ch2/index.html#history
     declarativeContents = {
+      # TODO: The example in the option for declarativeContents is broken.  The
+      # "dn=" prefix seems to be throwing it off.  I have not seen other
+      # configurations in the wild that use it.  I should open a ticket about
+      # it.
       "dc=proton,dc=org" = ''
-            dn= dn: dc=proton,dc=org
-            objectClass: dcObject
-            objectClass: organization
-            dc: proton
-            description: The proton network.
-            o: Proton Network
+        dn: dc=proton,dc=org
+        objectClass: domain
+        dc: proton
 
-            dn: ou=users,dc=proton,dc=org
-            objectClass = organizationalUnit
-            ou: users
-            description: Users in the proton network.
+        dn: ou=users,dc=proton,dc=org
+        objectClass: organizationalUnit
+        ou: users
+        description: Users in the proton network.
 
-            dn: logan,ou=users,dc=proton,dc=org
-            objectClass: inetOrgPerson
-            cn: Logan Barnett
-            cn: Logan
-            sn: logan
-            uid: logan
-            mail: logustus@gmail.com
-            description: The reason we suffer.
-            ou: Administrators
-          '';
+        dn: uid=logan,ou=users,dc=proton,dc=org
+        objectClass: inetOrgPerson
+        cn: Logan Barnett
+        cn: Logan
+        sn: logan
+        uid: logan
+        mail: logustus@gmail.com
+        description: The reason we suffer.
+        ou: Administrators
+      '';
     };
+
     settings = {
       attrs = {
         olcLogLevel = "conns config";
@@ -61,31 +65,32 @@
           "${pkgs.openldap}/etc/schema/cosine.ldif"
           "${pkgs.openldap}/etc/schema/inetorgperson.ldif"
         ];
-        "olcDatabase={1}mdb".attrs = {
-          objectClass = [ "olcDatabaseConfig" "olcMdbConfig" ];
-          olcDatabase = "{1}mdb";
-          olcDbDirectory = "/var/lib/openldap/data";
-          olcSuffix = "dc=proton,dc=org";
-          /* your admin account, do not use writeText on a production system */
-          olcRootDN = "cn=admin,dc=proton,dc=org";
-          # Untested.  Should work but no run done yet.
-          olcRootPW.path = config.age.secrets.ldap-root-pass.path;
-          olcAccess = [
-            /* custom access rules for userPassword attributes */
-            ''{0}to attrs=userPassword
+        "olcDatabase={1}mdb" = {
+          attrs = {
+            objectClass = [ "olcDatabaseConfig" "olcMdbConfig" ];
+            olcDatabase = "{1}mdb";
+            olcDbDirectory = "/var/lib/openldap/data";
+            olcSuffix = "dc=proton,dc=org";
+            /* your admin account, do not use writeText on a production system */
+            olcRootDN = "cn=admin,dc=proton,dc=org";
+            # Untested.  Should work but no run done yet.
+            olcRootPW.path = config.age.secrets.ldap-root-pass.path;
+            olcAccess = [
+              /* custom access rules for userPassword attributes */
+              ''{0}to attrs=userPassword
                 by self write
                 by anonymous auth
                 by * none''
-            /* allow read on anything else */
-            ''{1}to *
+              /* allow read on anything else */
+              ''{1}to *
                 by * read''
-          ];
+            ];
+          };
 
-          # TODO: Test this now that these are nested under children for
-          # `olcDatabase={1}mdb`.  I just now followed that from the wiki page,
-          # where just before I had incorrectly copied it.
           children = {
 
+            # ppolicy is no longer needed?  I've read some things indicating
+            # it's not needed, but this doesn't error so who knows.
             "olcOverlay={2}ppolicy".attrs = {
               objectClass = [ "olcOverlayConfig" "olcPPolicyConfig" "top" ];
               olcOverlay = "{2}ppolicy";
@@ -105,13 +110,23 @@
             "olcOverlay={4}refint".attrs = {
               objectClass = [ "olcOverlayConfig" "olcRefintConfig" "top" ];
               olcOverlay = "{4}refint";
-              olcRefintAttribute = "memberof member manager owner";
+              # TODO: Update documentation to use list instead of space
+              # separated string.
+              olcRefintAttribute = [
+                "memberof"
+                "member"
+                "manager"
+                "owner"
+              ];
             };
 
           };
         };
       };
     };
+  };
+  users.users.openldap = {
+    extraGroups = [ "tls-leaf" ];
   };
   /* ensure openldap is launched after certificates are created */
   # systemd.services.openldap = {
--- nix/nixos-modules/tls-leaf-proton.nix
+++ nix/nixos-modules/tls-leaf-proton.nix
@@ -11,18 +11,17 @@
       ];
       script = "tls-signed-certificate";
     };
-    # This will probably need to be revisited.  Conceivably more than one
-    # service might need to use this key, and so we should probably have a group
-    # that indicates access just to this one file, and then add those services
-    # to that group.  For now though, just give it to nginx as it _should_ be
-    # the only thing to need it.
-    group = "nginx";
+    group = "tls-leaf";
     mode = "0440";
     settings = {
       root-certificate = config.age.secrets.proton-ca;
       fqdn = "${host-id}.proton";
     };
     rekeyFile = ../secrets/tls-${host-id}.key.age;
+  };
+
+  users.groups = {
+    tls-leaf = {};
   };
 
 }
