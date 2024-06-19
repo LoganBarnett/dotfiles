42a2741978f4221f5cd271c8c608298ddc0e8c47
LoganBarnett <logustus@gmail.com>
2024-06-19 15:33:00 -0700

    fix nvidia/cuda not working

--- nix/nixos-modules/nvidia.nix
+++ nix/nixos-modules/nvidia.nix
@@ -18,13 +18,16 @@ in {
   ];
   # The open source module doesn't support my card.  See
   # https://github.com/NVIDIA/open-gpu-kernel-modules for supported cards.  For
-  # some reason, `lshw -c display` shows `nouveau` for the display driver, even
-  # though I have `hardware.nvidia.open = false`.  That said, this can happen
-  # because a reboot wasn't done.  See
+  # some reason, `lshw -c display` may show `nouveau` for the display driver,
+  # even though I have `hardware.nvidia.open = false`.  That said, this can
+  # happen because a reboot wasn't done.  See
   # https://github.com/NixOS/nixpkgs/issues/16711 someone who had the same
   # problem on a much older NixOS.
   boot.blacklistedKernelModules = ["nouveau"];
   environment.systemPackages = [
+    # Show us library paths used so we can see where errant libcuda
+    # installations may be.  It's not in by this package though.
+    # pkgs.ldd
     # Allow us to get the PCI Bus ID for the graphics card.  This will render a
     # litle differently than lspci, and nvidiaBusId demands a specific format
     # that's closer to what lshw puts out.  There might be a flag that would fix
@@ -33,13 +36,27 @@ in {
     # The wiki is also wrong about this package - it doesn't exist.
     # pkgs.nvidia-persistenced
     # pkgs.nvidia-settings
-    linux-packages.nvidia_x11
+    # This will also bring in `nvidia-smi` which can be helpful for seeing if
+    # the kernel driver and other things are compatible.  But beware mismatches
+    # happening from lack of restarts per:
+    # https://github.com/NixOS/nixpkgs/issues/255070
+    # Actually this doesn't bring in nvidia-smi and it can cause version
+    # conflicts between the kernel driver and the library!
+    # linux-packages.nvidia_x11
     # Includes `lspci`.  `lspci` gets more information about the PCI bus.
     # Useful for debugging issues with the driver not picking up the hardware.
     pkgs.pciutils
-    # Somehow this executable is available to some folks for diagnostic
-    # purposes, but this package apparently doesn't exist.
-    # pkgs.nvidia-smi
+    # saxpy can is the best "doctor" tool for version mismatches.  Example
+    # output of a version mismatch:
+    # [logan@lithium:~]$ saxpy
+    # Start
+    # Runtime version: 12020
+    # Driver version: 12050
+    # Host memory initialized, copying to the device
+    # Scheduled a cudaMemcpy, calling the kernel
+    # Scheduled a kernel call
+    # Max error: 0.000000
+    pkgs.cudaPackages.saxpy
     # If encountering "RuntimeError: No CUDA GPUs are available", use this to
     # debug:
     # LD_DEBUG=libs python -c "import torch ; torch.cuda.is_available()"
@@ -53,6 +70,11 @@ in {
     # [logan@lithium:~]$ nvidia-smi
     # NVIDIA-SMI has failed because it couldn't communicate with the NVIDIA
     # driver. Make sure that the latest NVIDIA driver is installed and running.
+    # Use `modinfo nvidia | grep -i version` to determine what the kernel
+    # version is.  `ls /run/opengl-driver/lib/libnvidia-nvvm.so.*` will show
+    # what version the libraries are.  There are a lot of other files in that
+    # directory with the version number, in case this vanishes in a future
+    # configuration.
   ];
   # I'd like to better understand what these are doing.
   imports = [
@@ -62,6 +84,7 @@ in {
     flake-inputs.nixos-hardware.nixosModules.common-gpu-nvidia-nonprime
   ];
   hardware.opengl.enable = true;
+  hardware.opengl.package = linux-packages.nvidiaPackages.beta;
   hardware.opengl.driSupport = true;
   hardware.opengl.driSupport32Bit = true;
   # Most of this can be found here:
@@ -80,7 +103,8 @@ in {
     # supported GPUs is at:
     # https://github.com/NVIDIA/open-gpu-kernel-modules#compatible-gpus
     # Only available from driver 515.43.04+
-    # Currently alpha-quality/buggy, so false is currently the recommended setting.
+    # Currently alpha-quality/buggy, so false is currently the recommended
+    # setting.
     open = false;
     # Enable the Nvidia settings menu,
     # accessible via `nvidia-settings`.
@@ -94,7 +118,8 @@ in {
     # prime.offload.enable = true;
     # Bus ID of the NVIDIA GPU. You can find it using lspci, either under 3D
     # or VGA.  Examples in the wild will show "PCI:1:0:0" but that's not the
-    # actual output of lspci.
+    # actual output of lspci.  `sudo lshw display` will show a terse and closer
+    # ID to what we need.
     prime.nvidiaBusId = "PCI:1:0:0";
   };
   # One can arrive at compiler errors like this:
@@ -103,6 +128,9 @@ in {
   # This indicates a lack of configuration on this NixOS module.  See
   # https://github.com/NixOS/nixpkgs/issues/248242 for the specifics that need
   # to be added.
+  #
+  # It could also mean the drivers don't match the libraries.  `nvidia-smi` can
+  # help determine if that's the case (a failure meaning it is a mismatch).
   nixpkgs.config = {
     # TODO: Use this to bless specific packages.
     # nixpkgs.config.allowUnfreePredicate = pkg: builtins.elem (lib.getName pkg) [
