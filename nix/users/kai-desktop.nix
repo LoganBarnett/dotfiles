545795e9f1079a968eede95bf56ca18d04a510b3
Logan Barnett <logustus@gmail.com>
2024-09-06 17:45:14 -0700

    give Kai Mincraft and Minecraft-like launchers

--- nix/users/kai-desktop.nix
+++ nix/users/kai-desktop.nix
@@ -1,9 +1,26 @@
-{ ... }: {
+{ lib, pkgs, ... }: {
+  allowUnfreePackagePredicates = [
+    (pkg: builtins.elem (lib.getName pkg) [
+      "minecraft"
+      "minecraft-launcher"
+    ])
+  ];
+  imports = [
+    ../nixos-modules/unfree-predicates.nix
+  ];
   # Nothing to do here yet.
-  # home-manager.users.solomon = {};
+  # home-manager.users.kai = {};
   users.users.kai = {
     home = "/home/kai";
     isNormalUser = true;
     initialPassword = "appetizerrelatedabnormalbaboonpostalanime";
   };
+  environment.systemPackages = [
+    # An open source MineCraft launcher with some mod management features. An
+    # alternative is prismlauncher but this one sounded more featureful from a
+    # quick search.
+    pkgs.atlauncher
+    pkgs.minetest
+  ];
+
 }
