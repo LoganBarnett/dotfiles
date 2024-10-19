711d1bb29ec04985a0d6ab841e0bf46ef38fe2e6
Logan Barnett <logustus@gmail.com>
2024-10-18 19:41:27 -0700

    fix nix installer
    Lessons from trauma of doing a reinstall of Nix to get on Nix v2.24.9.

--- nix-install.sh
+++ nix-install.sh
@@ -24,7 +24,7 @@ yes | sh <(curl -L https://nixos.org/nix/install) \
   || true
 
 slog "Reloading daemon..."
-sudo launchctl remove org.nixos.nix-daemon
+sudo launchctl bootout system/org.nixos.nix-daemon
 # nix-env -iA nixpkgs.nix
 # The -w is necessary to fix an issue if the nix-daemon ever got disabled.  -w
 # forces the "Disabled" key to get rewritten. and thus prevents startup issues
@@ -60,7 +60,27 @@ slog "nix-daemon loaded."
 # sudo mkdir /nix/var/nix/profiles/per-user/$user
 # chown -R $user:nixbld /nix/var/nix/profiles/per-user/$user
 
-./nix-home-manager-install.sh
+# Work around potential TLS/SSL issues here:
+# https://discourse.nixos.org/t/ssl-ca-cert-error-on-macos/31171/5
+# Basically there's a lingering SSL cert symlink and it doesn't work.
+sudo rm /etc/ssl/certs/ca-certificates.crt
+# The advice says to symlink this file in, but the nix-darwin install fails with
+# this:
+# error: Unexpected files in /etc, aborting activation
+# The following files have unrecognized content and would be overwritten:
+#
+#   /etc/ssl/certs/ca-certificates.crt
+#
+# Please check there is nothing critical in these files, rename them by adding .before-nix-darwin to the end, and then try again.
+# sudo ln -s /nix/var/nix/profiles/default/etc/ssl/certs/ca-bundle.crt /etc/ssl/certs/ca-certificates.crt
+# Does this use my flake inputs or is it a "recent" nixpkgs?
+# There's no SSL certs installed at this point, so we're just hanging in the
+# breeze I guess.
+nix run nix-darwin -- switch --flake ".#$machineName"
+
+# We no longer directly install home manager but use it through nix-darwin
+# instead.
+# ./nix-home-manager-install.sh
 
 # The guide recommends the below flag for catalina.
 # https://nixos.org/manual/nix/stable/#chap-quick-start
--- nix-reinstall.sh
+++ nix-reinstall.sh
@@ -7,6 +7,15 @@
 # working nix installation once it's all done. This differs from the intent of
 # nix-install.sh in that the reinstall process is lengthy, and doesn't have a
 # focus on upgrading packages.
+#
+# I have found this is very much a moving target.  Make sure there's nothing
+# running from /nix because we need to unmount it.  This will require shutting a
+# lot of stuff down, and tearing down any shell tabs open.  You'll need a new
+# shell that forcibly uses /bin/zsh as its startup shell, since it's configured
+# to use the Nix version already.
+#
+# Additional tidbits might get updated here:
+# https://nix.dev/manual/nix/2.18/installation/uninstall.html#linux
 ####
 
 source nix/bash-logging
@@ -14,15 +23,24 @@ source nix/bash-logging
 slog "The first step is to make nix think we are worthy of install (no existing
 nix). We will remove everything! This script requires sudoer."
 
+slog "Disabling the Linux builder if it's running..."
+sudo launchctl bootout system/org.nixos.linux-builder
+
 slog "Disabling the nix-daemon..."
 
-sudo launchctl disable system/org.nixos.nix-daemon
+# TODO: Make this a macOS only step.
+sudo launchctl bootout system/org.nixos.nix-daemon
 
 slog "Hold onto your butts - no turning back now..."
 
+slog "Removing /nix volume..."
+sudo diskutil apfs deleteVolume /nix
+# These might be needed, but maybe not.
+# sudo diskutil unmount /nix
+# sudo diskutil unmount /private/var/run/agenix.d || true
+
 sudo rm -rf \
   /etc/nix \
-  /nix \
   /var/root/.nix-profile \
   /var/root/.nix-defexpr \
   /var/root/.nix-channels \
@@ -40,6 +58,28 @@ slog "Purged all nix files."
 slog "Restoring pre-nix system wide shell configs..."
 sudo mv /etc/bashrc{.backup-before-nix,}
 sudo mv /etc/zshrc{.backup-before-nix,}
+
+slog "Removing nixbuild group..."
+sudo dscl . -delete /Groups/nixbld
+
+slog "Removing old nixbuild users..."
+for u in $(sudo dscl . -list /Users | grep _nixbld); do
+  sudo dscl . -delete /Users/$u
+done
+
+slog "Removing nix entry in /etc/synthetic.conf..."
+sudo sed -i '/nix/d' /etc/synthetic.conf
+
+slog "Re-enabling nix-daemon and linux-builder to prep for nix-install.sh..."
+# Earlier we disabled the nix-daemon and linux-builder services.  This was to
+# help us remove stuff.  We must re-enable them now since the installer isn't
+# smart enough to do that.  See
+# https://github.com/NixOS/nix/issues/6499#issuecomment-1364200393 for
+# additional context.
+sudo launchctl enable system/org.nixos.nix-daemon
+sudo launchctl kickstart -k system/org.nixos.nix-daemon
+sudo launchctl enable system/org.nixos.linux-builder
+sudo launchctl kickstart -k system/org.nixos.linux-builder
 
 slog "At this point we should be free to run the install logic - delegating to
 nix-install.sh"
