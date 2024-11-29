44cbb9d28374dd946f254d2e79396baab8081f49
Logan Barnett <logustus@gmail.com>
2024-11-29 13:48:54 -0800

    note what's going on with wireguard generation

--- nix/hosts/copper.nix
+++ nix/hosts/copper.nix
@@ -14,6 +14,14 @@ in {
     (import ../nixos-modules/server-host.nix {
       inherit flake-inputs host-id system;
     })
+    # TODO: Right now agenix-rekey wants to build wireguard to do the
+    # generation.  This fails due to a problem with macOS building wireguard-go
+    # (documented in the overlay in this repository).  It is not understood why
+    # it's not using the overlays.  I've removed the root nixpkgs channel and it
+    # still provides this error, even with all the settings see in
+    # ../nixos-modules/nix-flake-environment.nix.  I'm at a loss here.  This
+    # probably warrants a ticket and I'll get to that sometime.  I have
+    # confirmed that commenting this out (at least temporarily) fixes the issue.
     (import ../nixos-modules/wireguard-server.nix {
       inherit host-id;
     })
