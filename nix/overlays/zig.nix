3473b3f914733a79ca054bc9429e774d597c147a
Logan Barnett <logustus@gmail.com>
2024-12-19 16:47:43 -0800

    remove zig workaround
    This seems to be breaking things now.

--- nix/overlays/zig.nix
+++ nix/overlays/zig.nix
@@ -2,13 +2,13 @@
 # This applies to a zig build failure.
 # zig is used by ncdu.
 final: prev: {
-  cmake = prev.cmake.overrideAttrs {
-    strictDeps = false;
-  };
-  libclang = prev.libclang.overrideAttrs {
-    strictDeps = false;
-  };
-  zig = prev.zig.overrideAttrs {
-    strictDeps = false;
-  };
+  # cmake = prev.cmake.overrideAttrs {
+  #   strictDeps = false;
+  # };
+  # libclang = prev.libclang.overrideAttrs {
+  #   strictDeps = false;
+  # };
+  # zig = prev.zig.overrideAttrs {
+  #   strictDeps = false;
+  # };
 }
