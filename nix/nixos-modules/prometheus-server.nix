0be0b002752ab9a4af4b4a644477ddfeb494f4c8
Logan Barnett <logustus@gmail.com>
2025-04-27 22:18:55 -0700

    add more Prometheus notes

--- nix/nixos-modules/prometheus-server.nix
+++ nix/nixos-modules/prometheus-server.nix
@@ -10,16 +10,24 @@
 # See https://wiki.nixos.org/wiki/Prometheus for a writeup on how to get started
 # with Prometheus in NixOS.
 #
+# ./prometheus-client.nix covers needs of the clients to run "exporters", which
+# are what aggregate and offer data that the server can poll.  This might seem
+# like the client/server nomenclature is backwards as I am using it.  Maybe it
+# is, but this server file is meant to represent the location where prometheus
+# aggregates and stores all of its exporters across all hosts.
+#
 # Configuring this module:
+#
 # The server must know about what exporters are available.  The convention used
 # in this module is to search for amongst the facts metadata to determine what
 # clients are out there and what exporters they use.
 #
 # While there may be many Prometheus exporters line up 1:1 with the monitor
-# names used in the facts, they aren't necessarily going to match fully - this
-# is to keep the nomenclature more about monitoring and less specifically about
-# Prometheus.  The facts should specify the intent and this module translates
-# that to Prometheus configuration.
+# names used in the facts (./facts.nix), they aren't necessarily going to match
+# fully - this is to keep the nomenclature more about monitoring and less
+# specifically about Prometheus.  The facts should specify the intent and this
+# module translates that to Prometheus configuration.  Also we might have a
+# monitor differ in name but share the same exporter.
 ################################################################################
 { config, facts, lib, pkgs, ... }: {
   services.prometheus = {
