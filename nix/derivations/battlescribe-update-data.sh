fbfa75d645f7fc4984b2c4875fbb3dc4cd00b9f3
Logan Barnett <logustus@gmail.com>
2025-04-27 22:24:13 -0700

    try to make battlescribe data loading work again, and fail
    They broke it on purpose, I think.

--- nix/derivations/battlescribe-update-data.sh
+++ nix/derivations/battlescribe-update-data.sh
@@ -2,14 +2,16 @@
 
 set -euo pipefail
 
+base_path="$HOME/Dropbox/Apps/BattleScribe/data"
 dir=$(mktemp --directory)
 cd $dir
 lastversion download https://github.com/BSData/wh40k-10e
 # -x for extract.  No long argument for it.
 tar --gunzip -x --file wh40k-*.tar.gz
 # Remove so the poor glob we have doesn't pick it up on the move.
-rm *.tar.gz
-data_path="$HOME/Dropbox/Apps/BattleScribe/data/Warhammer 40,000 10th Edition"
+# rm *.tar.gz
+mv *.tar.gz "$base_path"
+data_path="$base_path/Warhammer 40,000 10th Edition"
 rm -rf "$data_path.old"
 mv "$data_path" "$data_path.old"
 mkdir "$data_path"
