3a9828ebc766abc975a5a74ae55e551752e751e0
Logan Barnett <logustus@gmail.com>
2025-05-04 22:25:47 -0700

    make sure the DNS server can come back up on its own

--- nix/nixos-modules/dns-server.nix
+++ nix/nixos-modules/dns-server.nix
@@ -11,7 +11,53 @@
 # for dynamic DNS on the part of the server for any of our known hosts.  We can
 # simply allow the facts structure to dictate what we need.
 ################################################################################
-{ lib, facts, ... }: {
+{ lib, facts, host-id, ... }: let
+  # TODO: Make this a dynamic value on the host.
+  subnet = facts.network.subnets.barnett-main;
+  host-ip = hostname: host: "${subnet}.${toString host.ipv4}";
+  my-ip = host-ip host-id facts.network.hosts.${host-id};
+  # This is because the names vary.  Instead of trying to guess, just set them
+  # all.
+  forced-ip-interface-config = {
+    ipv4.addresses = [{
+      address = my-ip;
+      prefixLength = 24;
+    }];
+  };
+in {
+  networking.interfaces = {
+    # systemd "predictable".
+    enp3s0 = forced-ip-interface-config;
+    eno1 = forced-ip-interface-config;
+    # Firmware set.
+    end0 = forced-ip-interface-config;
+    ens0 = forced-ip-interface-config;
+    # Sometimes user forced.
+    eth0 = forced-ip-interface-config;
+  };
+  # TODO: Make dynamic some day.  Make it follow "gateway".
+  networking.defaultGateway = "${subnet}.254";
+  # Ugh I'm not sure how to make this work with my ISP's DNS.  Do I even want
+  # that?
+  networking.nameservers = let
+    # Just so we know whose these are.
+    cloudflare = "1.1.1.1";
+    opendns = "208.67.222.222";
+    quad9 = "9.9.9.9";
+    # I'd rather give Google the tracking capability than have no DNS.  But the
+    # other three failing is pretty unlikely.
+    google = "8.8.8.8";
+  in [
+    # Order is intentional.
+    opendns
+    quad9
+    cloudflare
+    google
+  ];
+  # Allow actual DNS and DHCP connections.
+  networking.firewall.allowedUDPPorts = [ 53 67 ];
+  # Larger connections (DNSSEC, zone transfers) use TCP for DNS.
+  networking.firewall.allowedTCPPorts = [ 53 ];
   services.dnsmasq = {
     enable = true;
     settings = {
@@ -23,12 +69,9 @@
         (lib.attrsets.filterAttrs (hostname: host:
           host ? ipv4 && host.ipv4 != null
         ))
-        (lib.attrsets.mapAttrsToList (hostname: host: (let
-          # TODO: Make this a dynamic value on the host.
-          subnet = facts.network.subnets.barnett-main;
-        in
-          "${hostname},${subnet}.${toString host.ipv4}"
-        )))
+        (lib.attrsets.mapAttrsToList (hostname: host:
+          "${hostname},${host-ip hostname host}"
+        ))
       ];
       # dhcp-host = [
       #   # Examples - make these dynamic.
@@ -38,7 +81,7 @@
       # ];
       dhcp-option = [
         "option:router,192.168.254.254"
-        "option:dns-server,192.168.254.254"
+        "option:dns-server,${my-ip}"
       ];
     };
   };
