63915748c1ac621455b4bfedf524fb9d1ef17fd3
Logan Barnett <logustus@gmail.com>
2025-06-02 16:49:07 -0700

    add trivial about the Copper element

--- nix/hosts/copper.nix
+++ nix/hosts/copper.nix
@@ -1,5 +1,7 @@
 ################################################################################
 # This defines the entirety of the configuration for the copper host.
+#
+# Trivia:  About 95% of the copper ever mined has been extracted since 1900.
 ################################################################################
 { flake-inputs, host-id, system, ... }: let
 in {
