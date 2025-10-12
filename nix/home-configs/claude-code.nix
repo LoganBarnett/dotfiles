12e8f127ec81118e06c91c0908bb74eafff8891f
Logan Barnett <logustus@gmail.com>
2025-10-11 23:29:44 -0700

    switch to sonnet, allow reading the read-only Nix store

--- nix/home-configs/claude-code.nix
+++ nix/home-configs/claude-code.nix
@@ -6,6 +6,18 @@
     # This doesn't actually work but it is listed in the global configuration
     # (claude config list --global).
     editorMode = "vim";
-    model = "claude-opus-4-1-20250805";
+    model = "claude-sonnet-4-5-20250929";
+    # model = "claude-opus-4-1-20250805";
+    mcp = {
+      allowedDirectories = {
+        read = [
+          # Allowing specific directories to be read is kind of a hassle,
+          # especially if we're looping over builds which will generally produce
+          # new directories in the store.  Just allow reading the whole thing,
+          # since nothing should ever be sensitive in there anyways.
+          "/nix/store"
+        ];
+      };
+    };
   };
 }
