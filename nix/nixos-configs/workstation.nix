583cdecfdea170c9d237ff09aa81176717c515df
Logan Barnett <logan.barnett@nwea.org>
2025-10-15 16:15:45 -0700

    use recent version of mcphost

--- nix/nixos-configs/workstation.nix
+++ nix/nixos-configs/workstation.nix
@@ -11,7 +11,22 @@
     # An interactive LLM runner with MCP support.  Written in Go, so no runtime
     # and weird build dependencies.  It supports various debugging options as
     # well.  See https://github.com/mark3labs/mcphost for details.
-    pkgs.mcphost
+    (pkgs.mcphost.overrideAttrs (old: let
+      version = "0.31.1";
+    in {
+      inherit version;
+      src = pkgs.fetchFromGitHub {
+        owner = "mark3labs";
+        repo = "mcphost";
+        rev = "v${version}";
+        hash = "sha256-WvoBQLrs9lcbaTueyIPI89Y2t37mKxlE0tOczLhw8Bg=";
+      };
+      # There seems to be some files expected to be laid down for the tests to
+      # work, such as `$HOME/.mcphost.yml`.  Just skip them until a hero can fix
+      # all of it.
+      doCheck = false;
+      vendorHash = "sha256-xZ7JbX1sAt2ZtgSMm86MQEJWwjL17O9LMsjOmkhzWt0=";
+    }))
   ];
 
 }
