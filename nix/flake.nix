d6daecc49bc9b1d0ca940360486e6965e02f001d
Logan Barnett <logustus@gmail.com>
2026-02-12 14:40:48 -0800

    scoot tea package override to where it is consumed

--- nix/flake.nix
+++ nix/flake.nix
@@ -273,16 +273,6 @@
                   .${system}
                   .nixos-option
                 ;
-                # Override tea to build from PR #897 branch which
-                # fixes TTY prompting in non-interactive environments.
-                tea = prev.tea.overrideAttrs (oldAttrs: {
-                  src = prev.fetchgit {
-                    url = "https://gitea.com/gitea/tea";
-                    rev = "6c5811e4e9241b16376beb8eb9138d7951d6090f";
-                    hash =
-                      "sha256-JCvwvVxJGSzr+L+OMfXIeeYsKi+m0PSS83BJ97U4u0c=";
-                  };
-                });
               })
             ];
           }
--- nix/nixos-modules/user-can-develop.nix
+++ nix/nixos-modules/user-can-develop.nix
@@ -17,7 +17,20 @@
     # version.
     pkgs.git
     # CLI tool for interacting with Gitea servers.
-    pkgs.tea
+    # Override tea to build from PR #897 branch which fixes TTY prompting in
+    # non-interactive environments.
+    (pkgs.tea.overrideAttrs (oldAttrs: {
+      src = pkgs.fetchgit {
+        url = "https://gitea.com/gitea/tea";
+        rev = "6c5811e4e9241b16376beb8eb9138d7951d6090f";
+        hash = "sha256-JCvwvVxJGSzr+L+OMfXIeeYsKi+m0PSS83BJ97U4u0c=";
+      };
+      # Set vendorHash to fetch fresh dependencies instead of using
+      # the out-of-sync vendor directory in the source.
+      vendorHash = "sha256-+s2uF8xfEoy+0fReRs7ftNFeQklNUr9AeFa9VKjaKas=";
+      # Disable tests since one tries to create config file in sandbox.
+      doCheck = false;
+    }))
     # A spell checker.
     pkgs.ispell
     # IBM font that looks good outside of source code contexts.
