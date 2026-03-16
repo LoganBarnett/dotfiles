a6f90a20947a87b1a62b296a24b3ce237c677ae0
Logan Barnett <logan.barnett@nwea.org>
2026-03-16 10:56:31 -0700

    put ollama in the relevant config

--- darwin-configs/ollama.nix
+++ darwin-configs/ollama.nix
@@ -13,6 +13,7 @@
 ################################################################################
 {
   config,
+  lib,
   lib-custom,
   pkgs,
   ...
@@ -33,6 +34,25 @@ in
 
   age.secrets.ollama-webhook-token = {
     generator.script = "base64";
+  };
+
+  services.ollama = {
+    enable = true;
+    host = "0.0.0.0";
+    # Pull in fix from https://github.com/NixOS/nixpkgs/pull/467197 with
+    # light adaptation.  This didn't make it into the 25.11 release.
+    package = pkgs.ollama.overrideAttrs (old: {
+      postPatch = ''
+        substituteInPlace version/version.go \
+          --replace-fail 0.0.0 '${old.version}'
+        rm -r app
+      ''
+      # disable tests that fail in sandbox due to Metal init failure
+      + lib.optionalString pkgs.stdenv.hostPlatform.isDarwin ''
+        rm ml/backend/ggml/ggml_test.go
+        rm ml/nn/pooling/pooling_test.go
+      '';
+    });
   };
 
   services.webhook = {
