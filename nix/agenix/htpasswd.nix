fba60fa0a8bf2add348018c1033776778622d089
Logan Barnett <logustus@gmail.com>
2026-03-15 19:06:31 -0700

    insert just the right amount of line breaks for htpasswds

--- nix/agenix/htpasswd.nix
+++ nix/agenix/htpasswd.nix
@@ -8,8 +8,16 @@
     secret,
     ...
   }:
-    lib.flip lib.concatMapStrings deps ({ file, ... }: ''
-      ${decrypt} ${lib.escapeShellArg file} \
-        | ${pkgs.apacheHttpd}/bin/htpasswd -niBC 10 ${lib.escapeShellArg secret.settings.username}
-    '');
+    # htpasswd flags:
+    #   -n: output to stdout.
+    #   -i: read password from stdin.
+    #   -B: bcrypt hashing.
+    #   -C: bcrypt cost factor.
+    # htpasswd outputs "username:hash\n" to stdout.  Use printf to avoid the
+    # heredoc adding an extra trailing newline.
+    lib.strings.concatMapStrings ({ file, ... }:
+      "printf '%s\\n' \"$(${decrypt} ${lib.escapeShellArg file} " +
+      "| ${pkgs.apacheHttpd}/bin/htpasswd -niBC 10 " +
+      "${lib.escapeShellArg secret.settings.username})\"; "
+    ) deps;
 }
