b7f887dd923cb623f9002f22ab87865c3ca5986f
Logan Barnett <logustus@gmail.com>
2026-03-22 19:19:49 -0700

    add new gitAdd helper

--- agenix/wireguard-priv.nix
+++ agenix/wireguard-priv.nix
@@ -1,11 +1,19 @@
 { lib, pkgs, ... }:
 {
   age.generators.wireguard-priv =
-    { pkgs, file, ... }:
+    {
+      pkgs,
+      file,
+      gitAdd,
+      ...
+    }:
+    let
+      pub-file = lib.escapeShellArg (lib.removeSuffix ".age" file + ".pub");
+    in
     ''
       priv=$(${pkgs.wireguard-tools}/bin/wg genkey)
-      ${pkgs.wireguard-tools}/bin/wg pubkey <<< "$priv" \
-        > ${lib.escapeShellArg (lib.removeSuffix ".age" file + ".pub")}
+      ${pkgs.wireguard-tools}/bin/wg pubkey <<< "$priv" > ${pub-file}
+      ${gitAdd} ${pub-file}
       echo -n "$priv"
     '';
 }
