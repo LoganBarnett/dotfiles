5f02239fd2c960404d280b8cd5434d06226083bf
Logan Barnett <logustus@gmail.com>
2026-04-08 16:36:52 -0700

    add some slim documentation for adding secrets to a service

--- agenix/secret-template-file.nix
+++ agenix/secret-template-file.nix
@@ -2,8 +2,7 @@
 # A agenix-rekey generator for using a template to substitute secrets into a
 # file that itself is also considered secret.
 #
-# This generator takes advantage of the un-merged settings feature in
-# agenix-rekey.
+# Secrets generated here should be added to the service's `restartTriggers`.
 #
 # The settings structure expected is:
 # {
@@ -17,35 +16,33 @@
 # The template string is in the form of %name% but one day I may make that
 # configurable.
 ################################################################################
-{ lib, ... }: {
-  age.generators.template-file = {
-    decrypt,
-    deps,
-    file,
-    name,
-    pkgs,
-    secret,
-    ...
-  }: let
-    template = (
-      secret.settings.template
-        or (builtins.readFile secret.settings.templateFile)
-    );
-  in
-   ''
-     printf '%s' ${lib.escapeShellArg template} \
-       ${
-         lib.strings.concatStringsSep " " (builtins.map
-           (dep: ''
-             | ${pkgs.replace}/bin/replace-literal \
-               -e \
-               -f \
-               "%${lib.escapeShellArg dep.name}%" \
-               "$(${decrypt} ${lib.escapeShellArg dep.file})" \
-           ''
-           )
-           deps
-         )
-       }
-   '';
+{ lib, ... }:
+{
+  age.generators.template-file =
+    {
+      decrypt,
+      deps,
+      file,
+      name,
+      pkgs,
+      secret,
+      ...
+    }:
+    let
+      template = (
+        secret.settings.template or (builtins.readFile secret.settings.templateFile)
+      );
+    in
+    ''
+      printf '%s' ${lib.escapeShellArg template} \
+        ${lib.strings.concatStringsSep " " (
+          builtins.map (dep: ''
+            | ${pkgs.replace}/bin/replace-literal \
+              -e \
+              -f \
+              "%${lib.escapeShellArg dep.name}%" \
+              "$(${decrypt} ${lib.escapeShellArg dep.file})" \
+          '') deps
+        )}
+    '';
 }
